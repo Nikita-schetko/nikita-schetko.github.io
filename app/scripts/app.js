831bb65c5119fc93ca900a139bfba671c416905c
Nikita Shchetko <Nikita Shchetko>
2017-01-14 22:51:23 +0300

    First check

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularVideoAppApp
+ * @description
+ * # angularVideoAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularVideoAppApp', []);
