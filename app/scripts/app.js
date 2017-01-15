660391e8c79eb5f6862fad5572d466edef3b2a94
Nikita-schetko <corrosion.by@gmail.com>
2017-01-15 12:59:03 +0300

    Angular logic written; AJAX request from server working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,16 @@
  * Main module of the application.
  */
 angular
-  .module('angularVideoAppApp', []);
+  .module('angularVideoAppApp', []).filter('youtubeEmbed', function () {
+    // value - данные для которых применяется фильтр
+    // toUpper - аргумент передаваемый фильтру
+    return function (value, toUpper) {
+        // проверка переменной value на наличие строки
+        if (angular.isString(value)) {
+            var processedValue = toUpper ? value.toUpperCase() : value.toLowerCase();
+            return processedValue;
+        } else {
+            return value;
+        }
+    };
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,47 +8,89 @@
  * Controller of the angularVideoAppApp
  */
 angular.module('angularVideoAppApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $sce) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    $scope.initilizeSlider = function (){
-            $('.your-class').slick({
-              dots: true, 
+    $scope.items = [];
+    $scope.items[0] = { title: 'Basic title', tagline: 'basic tagline', plot: 'test', trailerEmbed: 'https://www.youtube.com/embed/1MhPz88bqig' };
+    // trailer: 'http://youtube.com/watch?v=1MhPz88bqig'
+    $scope.currentPosition = 0;
+    $scope.currentItem = {};
+    $scope.$watch('currentPosition', function (newValue, oldValue) {
+      console.log('Старое значение - ' + oldValue + ', новое значение - ' + newValue);
+      $scope.currentItem = $scope.items[$scope.currentPosition];
+    });
+
+    $scope.nextItem = function () {
+      //Check, if we are at the end of array
+      $scope.currentPosition = ($scope.currentPosition === $scope.items.length - 1) ? 0 : $scope.currentPosition + 1;
+    };
+
+    $scope.prevItem = function () {
+      //Check, if we are at the beginning of array
+      $scope.currentPosition = ($scope.currentPosition === 0) ? $scope.currentPosition = $scope.items.length - 1 : $scope.currentPosition - 1;
+    };
+
+    $scope.trustSrc = function (src) {
+      return $sce.trustAsResourceUrl(src);
+    };
+
+    $scope.sendRequest = function () {
+      $http.get('https://le-taste.herokuapp.com/api/v1/movies/').then(function (response) {
+        console.log(response);
+        $scope.items = response.data;
+        for(var i=0; i<$scope.items.length-1; i++)
+        {
+          var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+          var youtubeBaseURL = 'https://youtube.com/embed/';
+          var videoId = '';
+          var matchedArray = [];
+          var currentElement = $scope.items[i];
+          matchedArray = currentElement.trailer.match(rx);
+          currentElement.trailerEmbed = youtubeBaseURL + matchedArray[1];   
+        }
+        $scope.currentItem = $scope.items[0];
+      });
+    };
+
+    $scope.initilizeSlider = function () {
+      $('.your-class').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
               infinite: true,
-              speed: 300,
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              responsive: [
-                {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                  }
-                },
-                {
-                  breakpoint: 600,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 480,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-              ]
-            });
-            };
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: 'unslick'
+          // instead of a settings object
+        ]
+      });
+    };
   });
