087ffc6b822837c80f5eb8e923d12580ffcf49af
Nikita-schetko <nschetko@Evoke.by>
2017-01-16 14:28:42 +0300

    Image backdrops; "Watched" button fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,14 +20,14 @@ angular.module('angularVideoAppApp')
     $scope.currentItem = {};
     $scope.currentPosition = 0;
     $scope.$watch('currentPosition', function (newValue, oldValue) {
-      console.log('Старое значение - ' + oldValue + ', новое значение - ' + newValue);
+      // console.log('Старое значение - ' + oldValue + ', новое значение - ' + newValue);
       $scope.currentItem = $scope.items[$scope.currentPosition];
     });
 
     $scope.nextItem = function () {
       //Check, if we are at the end of array
       $scope.currentPosition = ($scope.currentPosition === $scope.items.length - 1) ? 0 : $scope.currentPosition + 1;
-      $('.btn').blur(); 
+      $('.btn').blur();
       $('#watchBtnID').popover('hide');
     };
 
@@ -42,20 +42,25 @@ angular.module('angularVideoAppApp')
 
     $scope.sendRequest = function () {
       $http.get('https://le-taste.herokuapp.com/api/v1/movies/').then(function (response) {
-        console.log(response);
         $scope.items = response.data;
-        //Generating url for embed youtube videos
+        //Generating url for embed youtube videos and Generating image backdrops
         for (var i = 0; i < $scope.items.length - 1; i++) {
           var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
           var youtubeBaseURL = 'https://youtube.com/embed/';
           var videoId = '';
           var matchedArray = [];
           var currentElement = $scope.items[i];
+          // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+          currentElement.imageMainBackdrop = 'http://image.tmdb.org/t/p/w780/' + currentElement.backdrops[0].file_path;
+          // jscs:enable requireCamelCaseOrUpperCaseIdentifier
+
           if (currentElement.trailer === null)
           { break; }
           matchedArray = currentElement.trailer.match(rx);
           currentElement.trailerEmbed = youtubeBaseURL + matchedArray[1];
+
         }
+        console.log($scope.items);
         $scope.currentItem = $scope.items[0];
       });
     };
@@ -103,12 +108,15 @@ angular.module('angularVideoAppApp')
       var elem = ' <div class="btn-group"> <button type="button" class="btn btn-default" title="Hate that movie!" aria-label="Left Align"><span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span></button> <button type="button" class="btn btn-default" title="Its fine" aria-label="Center Align"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button> <button type="button" class="btn btn-default" title="Like this!" aria-label="Right Align"><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span></button> </div>';
       $('#watchBtnID').popover({ animation: true, content: elem, title: 'Do you like it?', trigger: 'click', html: true });
       $('#watchBtnID').on('shown.bs.popover', function () {
-            $('button.btn.btn-default').click(function (e) {
-                // angular.element(angularRegion).scope() - получение scope, который используется элементом разметки с id="angularRegion"
-                // $apply - применяет изменения на объекте scope
-                angular.element(this).scope().$apply('nextItem()');
-                
-            });
+        $('button.btn.btn-default').click(function (e) {
+          // angular.element(angularRegion).scope() - получение scope, который используется элементом разметки с id="angularRegion"
+          // $apply - применяет изменения на объекте scope
+          angular.element(this).scope().$apply('nextItem()');
+
+        });
+      });
+      $('body').on('hidden.bs.popover', function (e) {
+        $(e.target).data('bs.popover').inState = { click: false, hover: false, focus: false };
       });
     };
   });
